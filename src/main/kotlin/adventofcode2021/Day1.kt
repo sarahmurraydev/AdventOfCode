a4d68996174d3cbd4305cbccf8668f0e81b834ed
Sarah Murray <sarahmurraydev@gmail.com>
2021-12-02 11:38:23 -0500

    add day 1 part 2 solution and update readme progress

--- src/main/kotlin/adventofcode2021/Day1.kt
+++ src/main/kotlin/adventofcode2021/Day1.kt
@@ -6,9 +6,17 @@ fun main(){
     println("My List has ${myList.size} elements")
     val increases = checkListForIncreases(myList)
     println("My list increases item by item $increases times")
+
+    println("====================================")
+    println("Day 1, Part 2: Sets of 3")
+    val threeIncreases = checkListSetsForIncreases(myList)
+    println("My list increases sums of threes by $threeIncreases times")
 }
 
 /**
+ * ex list:
+ * val myList = listOf<Int>(0, 10, 5, 8, 6, 11, 14)
+ *
  * solution:
  * 0 --> 10 increased,
  * 10 --> 5 decreased,
@@ -50,4 +58,34 @@ fun getDataFromFile(): List<Int> {
     }
 
     return list
+}
+
+
+/**
+ * Part 2: Three sum window
+ * want to compare sum(i, i+1, i+2) with sum(i+1, i+2, i+3)
+ * we'll need to make two groups: i, i+1, i+2 and i+1, i+2, i+3
+ * since the last element n = size - 1 = [(n-3) + 3] .. we'll want to stop at size - 4
+ *
+ * ex list:
+ * val myList = listOf<Int>(0, 10, 5, 8, 6, 11, 14)
+ * set1 = 0, 10, 5 ==> sum = 15
+ * set2 = 10, 5, 8 ==> sum = 23 -- increased
+ * set3 = 5, 8, 6 ==> sum = 19
+ * set4 = 8, 6, 11 ==> sum = 25 -- increased
+ * set5 = 6, 11, 14 ==> sum = 31 -- increased
+ * ==> 3 increases
+ * */
+fun checkListSetsForIncreases(data: List<Int>): Int {
+    var numIncreases = 0
+
+    for(i in 0..data.size-4) {
+        val set1Sum = data[i] + data[i+1] + data[i+2]
+        val set2Sum = data[i+1] + data[i+2] + data[i+3]
+        if(set2Sum > set1Sum) {
+            numIncreases += 1
+        }
+    }
+
+    return numIncreases
 }
