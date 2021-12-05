756a2f2e79aba77b0f0e85588c4e9ed4a2d09277
Sarah Murray <sarahmurraydev@gmail.com>
2021-12-04 22:42:57 -0500

    find out when you get bingo and which card it's for

--- README.md
+++ README.md
@@ -15,7 +15,9 @@ Progress:
 |  | 2 | ✅ | 
 | 3 | 1 | ✅ | 51:23.70
 |  | 2 | ✅  | 1:38:19 ... ouch 
+| 3 | 1 | 🤔 | 10+ 
 
 for the record I've still gotten every thing right on the first try
 
 day 3 was brutal, I gotta go back and clean that code up. I got the answer but brutally and very messy
+day 3 ranking: 34) 401 ****  sarahmurraydev
---
+++ src/main/kotlin/adventofcode2021/Day4.kt
@@ -0,0 +1,86 @@
+package adventofcode2021
+
+fun main(){
+    println("Advent of Code Day 3!")
+    println("=====================")
+    println("====== Example Data: =======")
+
+    println(gotBingo(exBingoCards, exNumbersCalled))
+    println("====== Actual Data: =======")
+    //val day4Data = getDataFromFileAsStringList(4)
+
+    println("============================================")
+    println("====== PART 2 =======")
+    println("============================================")
+    println("====== Example Data: =======")
+    println()
+    println("====== Actual Data: =======")
+    println()
+    println("============================================")
+}
+
+// bingo
+// need to keep track of what numbers have been called / marked on the board
+// need to keep track of what numbers are needed to get bingo
+
+// could loop through each matrix
+// if matrix has number called, pop it off the array
+// check that the matrix row has length > 0
+// check that the matrix column has length > 0
+
+val exNumbersCalled = listOf(7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1)
+
+val exBingoCards = mutableListOf(
+    mutableListOf(22, 13, 17, 11,  0),
+    mutableListOf(8,  2, 23,  4, 24),
+    mutableListOf(21,  9, 14, 16,  7),
+    mutableListOf(6, 10,  3, 18,  5),
+    mutableListOf(1, 12, 20, 15, 19),
+
+    mutableListOf(3, 15,  0,  2, 22),
+    mutableListOf(9, 18, 13, 17,  5),
+    mutableListOf(19,  8,  7, 25, 23),
+    mutableListOf(20, 11, 10, 24,  4),
+    mutableListOf(14, 21, 16, 12,  6),
+
+    mutableListOf(14, 21, 17, 24,  4),
+    mutableListOf(10, 16, 15,  9, 19),
+    mutableListOf(18,  8, 23, 26, 20),
+    mutableListOf(22, 11, 13,  6,  5),
+    mutableListOf(2,  0, 12,  3,  7)
+)
+
+fun gotBingo(bingoCards: MutableList<MutableList<Int>>, numbersCalled: List<Int>): String {
+    for (num in numbersCalled) {
+        var rowSum = 0
+        var colSum = 0
+        var sumOfRows = MutableList(5) { 0 }
+        for (row in 0..4) {
+            for (col in 0..4) {
+                if (bingoCards[row][col] == numbersCalled[num]) {
+                    bingoCards[row][col] = 100
+                }
+
+                // after element has been set to 100, check what the sum of the row is
+                rowSum += bingoCards[row][col]
+                colSum += bingoCards[col][row]
+
+                println("row sum: $rowSum")
+
+                if (rowSum == 500 || colSum == 500) {
+                    println("we got bingo!")
+                    println("In card number ${(row % 5) + 1}")
+                    println("On number ${numbersCalled[num]}")
+                    println("It took ${num + 1 } numbers to be called before we got bingo")
+                    println("sumOfRows: $sumOfRows, ${sumOfRows.sum()}")
+                    return "Bingo!"
+                }
+            }
+            // now get the sum of all the unmarked numbers on the board
+            // this should be a sum of row sums mod 100
+            sumOfRows[row] = rowSum
+        }
+    }
+    return "Bingo!"
+}
+
